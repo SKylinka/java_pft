719edc515b854b771e5daadb6637e087a834a117
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-03-05 13:48:35 +0300

    Задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,8 +14,8 @@ public class MyFirstProgram {
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
 
-		Point p1 = new Point(1, 1);
-		Point p2 = new Point(2, 2);
+		Point p1 = new Point(1, 0);
+		Point p2 = new Point(2, 0);
 		System.out.println("Расстояние между первой точкой (" + p1.x + "," + p1.y + ") и второй (" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
 
 	}
