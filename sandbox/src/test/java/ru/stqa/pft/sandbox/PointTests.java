52b0dd2fc6a2ea043939510aa8308b7b2511a210
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-03-08 11:52:56 +0300

    исправление ошибок задания №3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,8 +6,27 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-  public void testArea() {
-    Square s = new  Square(5);
-    Assert.assertEquals(s.area(), 25.0);
+  public void testPointTrue1() {
+    Point p1 = new Point(1, 0);
+    Point p2 = new Point(2, 0);
+    Assert.assertEquals(p1.distance(p2), 1.0);
+  }
+  @Test
+  public void testPointTrue2() {
+    Point p1 = new Point(1, 0);
+    Point p2 = new Point(2, 0);
+    Assert.assertNotEquals(p1.distance(p2), 2.0);
+  }
+  @Test
+  public void testPointTrue3() {
+    Point p1 = new Point(3, 3);
+    Point p2 = new Point(4, 4);
+    Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+  }
+  @Test
+  public void testPointFalse() {
+    Point p1 = new Point(3, 3);
+    Point p2 = new Point(4, 4);
+    Assert.assertNotEquals(p1.distance(p2), 1.0);
   }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -6,15 +6,8 @@ import org.testng.annotations.Test;
 public class SquareTests {
 
   @Test
-  public void testPointTrue() {
-    Point p1 = new Point(1, 0);
-    Point p2 = new Point(2, 0);
-    Assert.assertEquals(p1.distance(p2), 1.0);
-  }
-  @Test
-  public void testPointFalse() {
-    Point p1 = new Point(1, 0);
-    Point p2 = new Point(2, 0);
-    Assert.assertEquals(p1.distance(p2), 2.0);
+  public void testArea() {
+    Square s = new  Square(5);
+    Assert.assertEquals(s.area(), 25.0);
   }
 }
