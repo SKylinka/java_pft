1964b3922d8770720361772837784e6986e4f954
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-04-05 11:26:12 +0300

    Исправление задания №11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -30,4 +29,19 @@ public class ContactPhoneTests extends TestBase{
   public static String cleaned(String phone) {
     return phone.replaceAll("\\s","").replaceAll("[-()]","");
   }
+
+  @Test
+  public void testContactEmail() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(),contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
+  }
+
 }
