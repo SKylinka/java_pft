2646ff0b49dd76dabb8e88b7ed4c19400de0f342
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-04-10 13:35:58 +0300

    и снова комит

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -61,7 +61,7 @@ public class GroupDataGenerator {
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(GroupData.class);
-    xstream.alias("groups", List.class);
+    //xstream.alias("groups", List.class);
     String xml = xstream.toXML(groups);
     Writer writer = new FileWriter(file);
     writer.write(xml);
