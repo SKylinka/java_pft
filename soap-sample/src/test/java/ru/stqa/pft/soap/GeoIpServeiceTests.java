266dd31d2a152ae5bcb14dec7164ab4449bb860a
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-05-04 11:36:15 +0300

    Лекция 9.2 через wsimport

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServeiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServeiceTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.soap;
 
+import com.lavasoft.GeoIPService;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -8,13 +9,15 @@ public class GeoIpServeiceTests {
 
   @Test
   public void testMyIp(){
-    String geoIp = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("192.168.1.1");
-    assertEquals(geoIp, "<GeoIP><Country>RU</Country><State>77</State></GeoIP>");
+    String geoIp = new GeoIPService().getGeoIPServiceSoap()
+            .getIpLocation("192.168.1.1");
+    assertEquals(geoIp, "<GeoIP><Country>US</Country><State>CA</State></GeoIP>");
   }
 
-  @Test
+  @Test(enabled = false)
   public void testInvalidIp(){
-    String geoIp = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("192.168.1.xxx");
+    String geoIp = new GeoIPService().getGeoIPServiceSoap()
+            .getIpLocation("213.135.86.2xxx");
     assertEquals(geoIp, "<GeoIP><Country>RU</Country><State>77</State></GeoIP>");
   }
 }
