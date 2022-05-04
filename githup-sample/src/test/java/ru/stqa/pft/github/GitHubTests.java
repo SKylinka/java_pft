00aebf40f2a5f2fb26e16b9431388bbcecd3e525
SKylinka <96301666+SKylinka@users.noreply.github.com>
2022-05-04 12:05:45 +0300

    Лекция 9.6

---
+++ githup-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GitHubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("SKylinka", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
